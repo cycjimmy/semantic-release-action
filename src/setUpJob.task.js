ac4bc8539b92aa3455a1c49e371f01c7e309bbb9
cycjimmy <cycjimmy@gmail.com>
2020-01-10 16:31:20 +0800

    chore: add debug info

--- src/setUpJob.task.js
+++ src/setUpJob.task.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const core = require('@actions/core');
 const outputs = require('./outputs.json');
 
@@ -8,6 +9,9 @@ const outputs = require('./outputs.json');
 module.exports = async () => {
   // set outputs default
   core.setOutput(outputs.new_release_published, 'false');
+
+  core.debug('action_workspace: ' + path.resolve(__dirname, '..'));
+  core.debug('process.cwd: ' + process.cwd());
 
   return Promise.resolve();
 };
