b7db0cbbf5f42eb8fe027828c236fbc340b5429e
densogiaichned <dsian@posteo.de>
2025-07-04 14:23:59 +0200

    fix(cleanupNpmrc.task.js): Use @actions/io to remove .npmrc

--- src/cleanupNpmrc.task.js
+++ src/cleanupNpmrc.task.js
@@ -1,15 +1,9 @@
-const core = require('@actions/core');
-const exec = require('./_exec');
+const io = require('@actions/io');
 
 /**
  * Clean up `.npmrc` file in the repo after releasing
  * @returns {Promise<never>}
  */
 module.exports = async () => {
-  const {stdout, stderr} = await exec(`rm -f .npmrc`);
-  core.debug(stdout);
-
-  if (stderr) {
-    return Promise.reject(stderr);
-  }
+  await io.rmRF('.npmrc');
 };
