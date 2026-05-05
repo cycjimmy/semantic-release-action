48f96baaf993c650e98e7a8f2747e20f88707821
Leif Ødegård Uhlen <126719+leifode@users.noreply.github.com>
2026-05-05 11:36:58 +0200

    even more tweaks cjs->esm

--- src/setUpJob.task.js
+++ src/setUpJob.task.js
@@ -1,7 +1,7 @@
-import path, {dirname} from 'path';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import * as core from '@actions/core';
-import outputs from './outputs.json';
+import outputs from './outputs.json' with { type: 'json' };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,6 +16,4 @@ export default async () => {
 
   core.debug('action_workspace: ' + path.resolve(__dirname, '..'));
   core.debug('process.cwd: ' + process.cwd());
-
-  return Promise.resolve();
 };
